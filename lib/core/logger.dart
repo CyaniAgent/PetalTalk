6e5b8f8fa1f336f5ae44c677eea90cdeef6c829c
Yuna Ayase <wangyiworks@163.com>
2026-01-15 06:20:47 +0800

    Fixing an error in logger.dart.

--- lib/core/logger.dart
+++ lib/core/logger.dart
@@ -22,7 +22,7 @@ class AppLogger {
   late Logger _logger;
 
   /// 文件输出实例
-  late final FileOutput _fileOutput;
+  late final AppFileOutput _fileOutput;
 
   /// 当前日志级别
   Level _currentLevel = Level.error;
